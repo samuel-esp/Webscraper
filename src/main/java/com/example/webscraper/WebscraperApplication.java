8a1bb445695b9a142570f4fae13a5fb05e947f27
Samuel Esposito <40698384+samuel-esp@users.noreply.github.com>
2021-11-26 20:44:43 +0100

    menu added

--- src/main/java/com/example/webscraper/WebscraperApplication.java
+++ src/main/java/com/example/webscraper/WebscraperApplication.java
@@ -6,17 +6,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 @SpringBootApplication
 public class WebscraperApplication {
 
     public static void main(String[] args) throws InterruptedException, IOException {
+
         SpringApplication.run(WebscraperApplication.class, args);
-        //CompanyScraperService companyScraperService = new CompanyScraperService();
-        //companyScraperService.scrape();
-        TennisPlayerService tennisPlayerService = new TennisPlayerService();
-        tennisPlayerService.scrape();
+
+        System.out.println("TYPE 1 TO SCRAPE YELP.COM ...\n");
+        System.out.println("TYPE 2 TO SCRAPE ATPTOUR.COM ...\n");
+        System.out.println("TYPE 3 TO SCRAPE TRUSTPILOT.COM ...\n");
+        System.out.println("TYPE 3 TO SCRAPE FANTACALCIO.IT ...\n");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+            if (Integer.parseInt(str) == 1) {
+
+                CompanyScraperService companyScraperService = new CompanyScraperService();
+                companyScraperService.scrape();
+
+            } else if (Integer.parseInt(str) == 2) {
+
+                TennisPlayerService tennisPlayerService = new TennisPlayerService();
+                tennisPlayerService.scrape();
+
+            } else if (Integer.parseInt(str) == 3) {
+
+
+            } else {
+                System.out.println("YOUR INPUT IS NOT VALID ...");
+            }
+
     }
 
 }
